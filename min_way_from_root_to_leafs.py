97f1f4ea44e4be32c4ee1927ca8f732068a5d083
Evgenii Kegeles <zhenyakeg@gmail.com>
2017-05-09 20:40:39 +0300

    initial commit

--- min_way_from_root_to_leafs.py
+++ min_way_from_root_to_leafs.py
@@ -6,6 +6,56 @@ class Node():
 
 
 class Tree():
+
     def __init__(self, root=None):
         self.root = root
+
+    def add(self, insertion, start="start"):
+        if start == "start":
+            start = self.root
+        if start is None:
+            self.root = Node(insertion)
+            return
+        if insertion > start.data:
+            if start.right is None:
+                start.right = Node(insertion)
+                return
+            else:
+                self.add(insertion, start.right)
+        elif insertion < start.data:
+            if start.left is None:
+                start.left = Node(insertion)
+                return
+            else:
+                self.add(insertion, start.left)
+
+
+    def print(self, start="start"):
+        if start == "start":
+            start = self.root
+        if start is None:
+            return
+        self.print(start.left)
+        print(start.data)
+        self.print(start.right)
+
+    def min_dist(self, start="start"):
+        if start == "start":
+            start = self.root
+        if start is None or (start.left is None and start.right is None):
+            return 0
+        return min(self.min_dist(start.left), self.min_dist(start.right)) + 1
+
+    def max_dist(self, start="start"):
+        if start == "start":
+            start = self.root
+        if start is None or (start.left is None and start.right is None):
+            return 0
+
+        return max(self.max_dist(start.left), self.max_dist(start.right)) + 1
+tree = Tree()
+for num in input().split():
+    tree.add(int(num))
+print(tree.min_dist() + tree.max_dist())
+
 
--- shortest_cycle.py
+++ shortest_cycle.py
@@ -5,32 +5,36 @@ for j in range(m):
     graph[v1] = graph.get(v1, []) + [v2]
 
 
-def bfs(graph, start, path):
-    used = set()
-    Q = []
-    used.add(start)
-    Q.append(start)
-    while Q:
-        vertex = Q.pop(0)
-        if len(path.get(start, [])) < 1 or vertex in graph.get(path.get(start, [])[-1], []):
-            path[start] = path.get(start, []) + [vertex]
-        for neighbour in graph.get(vertex, []):
-            if neighbour not in used:
-                Q.append(neighbour)
-                used.add(vertex)
-            else:
-                path[start] = path.get(start, []) + [neighbour]
-                return path
-    path[start] = []
-    return path
+def shortest_cycle(graph):
+    min_time = float('inf')
+    resulting_path = []
+    for start in graph:
+        used = set()
+        path = {}
+        Q = []
+        used.add(start)
+        Q.append((start, 0))
+        path[start] = [start]
+        while Q:
+            vertex, time = Q.pop(0)
+            time += 1
+            if time >= min_time:
+                break
+            for neighbour in graph.get(vertex, []):
+                if neighbour not in used:
+                    Q.append((neighbour, time))
+                    used.add(vertex)
+                    path[neighbour] = path[vertex] + [neighbour]
+                elif neighbour == start:
+                    min_time = time
+                    resulting_path = path[vertex] + [start]
+                    break
+            path.pop(vertex)
+        path[start] = []
+    return resulting_path
 
-def find_shortest_cycle(graph):
-    path = {}
-    for node in graph:
-        path = bfs(graph, node, path)
-    return(min(path.values(), key=lambda x: len(x)))
-res = ' '.join(map(str, find_shortest_cycle(graph)))
-if len(res) == 0:
-    print(-1)
+res = shortest_cycle(graph)
+if res:
+    print(' '. join(map(str, res)))
 else:
-    print(res)
+    print(-1)
